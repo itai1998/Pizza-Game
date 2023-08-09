9148e18b9d111327d5f719bc358c0a8368d2e6d5
I-Tai <smes93434@gmail.com>
2023-08-09 09:17:07 -1000

    Second commit

--- init.js
+++ init.js
@@ -1,3 +1,5 @@
+// Immediately Invoked Function Expression (IIFE, pronounced "iffy"). 
+// IIFE is a function that is defined and then immediately called
 (function(){
     const overworld = new Overworld({
         element: document.querySelector(".game-container")
